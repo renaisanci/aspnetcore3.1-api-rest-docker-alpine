03b1118708f961c90fe672a64bd71d258acf8111
Renato Fonseca <renaisanci@gmail.com>
2020-11-11 17:50:12 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste Renato Fonseca Azure Web App For Containers - Continuos deployment trigger enabled",
+                    Local = "CI/CD Docker container images",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
