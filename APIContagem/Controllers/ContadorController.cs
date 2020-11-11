09b2f6f8b1e982cd6772410b55c69cfdea6697ac
Renato Fonseca <renaisanci@gmail.com>
2020-11-11 18:00:41 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "CI/CD Docker container images",
+                    Local = "CI/CD Docker container images trigger deploy",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
